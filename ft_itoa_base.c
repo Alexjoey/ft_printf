204d6172538dcfa6d4c3bb3bdf8156faa7f266dc
Alex Mylle <alexm@live.be>
2023-12-11 18:59:20 +0100

    simplified ft_itoa base

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,35 +6,27 @@
 /*   By: amylle <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 13:21:29 by amylle            #+#    #+#             */
-/*   Updated: 2023/11/04 15:20:42 by amylle           ###   ########.fr       */
+/*   Updated: 2023/12/11 18:57:21 by amylle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static size_t	ft_len(unsigned long long n, char *base)
-{
-	size_t				len;
-	unsigned long long	base_len;
-
-	len = 1;
-	base_len = ft_strlen(base);
-	while (n >= base_len)
-	{
-		n /= base_len;
-		len++;
-	}
-	return (len);
-}
-
 char	*ft_itoa_base(unsigned long long n, char *base)
 {
-	int		num_len;
-	int		base_len;
-	char	*str;
+	int					num_len;
+	unsigned long long	base_len;
+	char				*str;
+	unsigned long long	temp;
 
+	temp = n;
 	base_len = ft_strlen(base);
-	num_len = ft_len(n, base);
+	num_len = 1;
+	while (temp >= base_len)
+	{
+		temp /= base_len;
+		num_len++;
+	}
 	str = ft_calloc((num_len + 1), sizeof(char));
 	if (!str)
 		return (NULL);
